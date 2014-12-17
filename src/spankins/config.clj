65b9e6b0a6c648354b70b5b9c39029f0360d7e5b
Matt Burns <mburns@dandb.com>
2014-12-17 15:54:29 -0800

    FNJEN-10: trying to figure out why uberjar stalling on compile

--- src/spankins/config.clj
+++ src/spankins/config.clj
@@ -1,4 +1,19 @@
 (ns spankins.config
-  (:require  [shoreleave.server-helpers :refer [safe-read]]))
+  ;;  (:require  [shoreleave.server-helpers :refer [safe-read]])
+  )
 
-(def cfg (safe-read (slurp (str (System/getProperty "user.dir") "/resources/spankins_config.edn"))))
+;;(def cfg (safe-read (slurp (str (System/getProperty "user.dir") "/resources/spankins_config.edn"))))
+(def cfg { :sql-statements "spankins/model/sql/queries.sql"
+          :export {:output-dir "resources/publish/output"}
+          :javascript {:3rd-party "resources/public/js/lib"
+                       :css "resources/public/css"
+                       :images "resources/public/css"}
+          :cljs "resources/public/"
+          :spankins-daemon-port 10333
+          :spankins-daemon-host "localhost"
+          :monitor-host "localhost"
+          :monitor-port 5555
+          :spankins-dsn {:classname "org.sqlite.JDBC"
+                         :subprotocol "sqlite"
+                         :subname "spankins.sqlite"}
+          })
